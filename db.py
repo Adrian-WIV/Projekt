a758c57cdef0ec495e2eee29118bc7c9b451fd55
Adrian-WIV <adrian.badar@rdf.nuernberg.de>
2025-05-31 12:43:20 +0200

    sql_einzelsuche

--- db.py
+++ db.py
@@ -53,4 +53,43 @@ def sql_einzelansicht(id="", vorname="", nachname="", produkte="", menge="", mon
     conn = mariadbconnect()
     cur = conn.cursor()
 
-    abfrage = """ SELECT  """
+    abfrage = """ SELECT 
+        kunden.IDKunde, 
+        kunden.Vorname, 
+        kunden.Name, 
+        produkte.Produktname, 
+        bestellposition.Menge, 
+        MONTH(bestellungen.Bestelldatum) AS Monat, 
+        YEAR(bestellungen.Bestelldatum) AS Jahr
+    FROM kunden 
+    JOIN bestellungen ON kunden.IDKunde = bestellungen.`kunden-id`
+    JOIN bestellposition ON bestellungen.`Bestell-ID` = bestellposition.bestell_ID
+    JOIN produkte ON bestellposition.Produkt_ID = produkte.`Produkt-ID`
+    WHERE 1=1 """
+
+    ausgabe = []
+
+    if vorname:
+        abfrage += "AND kunden.vorname LIKE ?"
+        abfrage.append(f"%{vorname}%")
+    
+    if nachname:
+        abfrage += "AND kunden.name LIKE ?"
+        abfrage.append(f"%{nachname}%")
+
+    if produkte:
+        abfrage += "AND produkte.produktname LIKE ?"
+        abfrage.append(f"%{produkte}%")
+
+    if monat:
+        abfrage += "AND MONTH(bestellungen.bestelldatum) LIKE ?"
+        abfrage.append(f"%{int(monat)}%")
+
+    if jahr:
+        abfrage += " AND YEAR(bestellungen.bestelldatum) LIKE ?"
+        abfrage.append(f"%{int(jahr)}%")
+
+    cur.execute(abfrage, tuple(ausgabe))
+    daten = cur.fetchall()
+    conn.close()
+    return daten
